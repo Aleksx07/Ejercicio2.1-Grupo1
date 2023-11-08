cc032b8b031122dd145c74d4c09811e2a19bbff2
Aleksx07 <134466359+Aleksx07@users.noreply.github.com>
2023-11-08 17:50:08 -0600

    modificacion

--- app/src/main/java/com/example/ejercicio21/ActivityListView.java
+++ app/src/main/java/com/example/ejercicio21/ActivityListView.java
@@ -41,27 +41,14 @@ public class ActivityListView extends AppCompatActivity {
 
     private void consultarLista() {
         SQLiteDatabase db = conexion.getReadableDatabase();
-        video video;
         listavideo = new ArrayList<>();
         Cursor cursor = db.rawQuery("SELECT * FROM " + transacciones.tablavideo, null);
 
         while (cursor.moveToNext()) {
-            video = new video();
+            video video = new video();
 
-            // Obtén el Blob desde el cursor
-            // Aquí asumo que la columna se llama "video" y es de tipo Blob
-            // Si no es así, ajusta el nombre de la columna y el tipo de dato según tu implementación
-            // Blob blob = cursor.getBlob(cursor.getColumnIndex("video"));
-
-            // Convierte el Blob a URL
-            // Aquí debes implementar la lógica para convertir el Blob a una URL o cadena que represente la ubicación del video
-            // Esto dependerá de cómo estás almacenando tus videos y cómo deseas representarlos como URL
-            // Puede ser la ubicación de un archivo en el sistema de archivos, una URL de servidor, etc.
-            // Reemplaza esta llamada con tu lógica de conversión real
-            // String videoUrl = convertBlobToUrl(blob);
-
-            // Simulación de la URL (reemplaza esta línea con la llamada a convertBlobToUrl)
-            String videoUrl = "https://ejemplo.com/ruta/al/video.mp4";
+            // Obtén la URL del video desde el cursor
+            @SuppressLint("Range") String videoUrl = cursor.getString(cursor.getColumnIndex(transacciones.tablavideo));
 
             // Log para verificar la URL antes de agregarla a la lista
             Log.d("ConsultaLista", "Video URL: " + videoUrl);
@@ -72,17 +59,6 @@ public class ActivityListView extends AppCompatActivity {
 
         cursor.close();
         obtenerLista();
-    }
-
-
-    // Método para convertir Blob a URL (debes implementar este método)
-    private String convertBlobToUrl(Blob blob) {
-        // Implementa la lógica para convertir el Blob a una URL o cadena que represente la ubicación del video
-        // Esto dependerá de cómo estás almacenando tus videos y cómo deseas representarlos como URL
-        // Puede ser la ubicación de un archivo en el sistema de archivos, una URL de servidor, etc.
-        // Retorna la URL como una cadena
-        // Ejemplo de implementación:
-        return "https://ejemplo.com/ruta/al/video.mp4";
     }
 
     private void obtenerLista() {
